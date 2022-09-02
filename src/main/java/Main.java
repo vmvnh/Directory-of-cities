d689d51fb2685455a04c9c2c46c4567b4270b46c
Yaroslav <yaroslav-w@mail.ru>
2022-09-02 23:28:22 +0300

    feat: Реализован вывод отсотированных списков.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,24 @@
 package main.java;
 
+import java.util.List;
+
 import static main.java.CityService.*;
+import static main.java.CityUtilities.*;
 
 public class Main {
 	public static void main(String[] args) {
 		String FILE_URL = "resources/cities.csv"; // Путь к файлу со списком городов
-		print(openFile(FILE_URL)); // Вывод списка городов в консоль
+		List<City> cities = openFile(FILE_URL);
 
+		print(cities); // Вывод списка городов в консоль
 
+		sortingByNameWithLambda(cities); // Сортировка массива по названию городов, используя lambda-выражения
+		print(cities); // Вывод списка городов в консоль
+
+		sortingByNameWithComparator(cities); // Сортировка массива по названию городов, используя java.util.Comparator
+		print(cities); // Вывод списка городов в консоль
+
+		sortingByDistrictAndName(cities); // Сортировка массива по федеральным округам и названиям городов в них
+		print(cities); // Вывод списка городов в консоль
 	}
 }
