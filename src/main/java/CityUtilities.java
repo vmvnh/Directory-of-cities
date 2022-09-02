b0f0ddc3aa9fa048de1bb373dc4335d375ae19f9
Yaroslav <yaroslav-w@mail.ru>
2022-09-02 23:43:07 +0300

    feat: Добавлены методы поиска города с наибольшим количеством жителей.

--- src/main/java/CityUtilities.java
+++ src/main/java/CityUtilities.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.text.MessageFormat;
 import java.util.Comparator;
 import java.util.List;
 
@@ -38,5 +39,33 @@ public class CityUtilities {
 	 */
 	public static void sortingByDistrictAndName(List<City> cities) {
 		cities.sort(Comparator.comparing(City::getDistrict).thenComparing(City::getName));
+	}
+
+	/**
+	 * Поиск города с наибольшим количеством жителей с помощью простого перебора.
+	 *
+	 * @param cities Массив городов.
+	 */
+	public static void findByBrutForce(List<City> cities) {
+		City[] citiesArray = cities.toArray(City[]::new);
+		City city = citiesArray[0];
+		int index = 0;
+
+		for (int i = 1; i < citiesArray.length; i++) {
+			if (citiesArray[i].getPopulation() > city.getPopulation()) {
+				city = citiesArray[i]; // Город содержащий наибольшее количество населения
+				index = i; // Индекс города в массиве
+			}
+		}
+		System.out.println(MessageFormat.format("[{0}] = {1}", index, citiesArray[index].getPopulation()));
+	}
+
+	/**
+	 * Поиск города с наибольшим количеством жителей с использованием lambda-выражений.
+	 *
+	 * @param cities Массив городов.
+	 */
+	public static void findMaxPopulation(List<City> cities) {
+		System.out.println(cities.stream().max(Comparator.comparing(City::getPopulation)));
 	}
 }
